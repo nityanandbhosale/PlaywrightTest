a3bf349a9d7395c9d6f4fbf6573ea56f33ad903a
bhosalenityanand <bhosalenityanand5@gmail.com>
2025-05-16 19:55:17 +0530

    add condition for dotenv when run on CI

--- playwright.config.js
+++ playwright.config.js
@@ -7,9 +7,11 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({
-  path: path.resolve(__dirname, '.env')
-});
+if (process.env.CI !== 'true') {
+  dotenv.config({
+    path: path.resolve(__dirname, '.env')
+  });
+}
 
 /**
  * @see https://playwright.dev/docs/test-configuration
